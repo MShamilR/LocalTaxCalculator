5e247bf690cec39b14f46fbfcbc619619797e27f
supremeincubator <mohammedshamil0300@gmail.com>
2022-10-22 20:32:00 +0530

    Created divs for input

---
+++ index.js
@@ -0,0 +1,6 @@
+const perMonthValue = document.querySelector(".monthValue");
+const perAnnumValue = document.querySelector(".annumValue");
+
+perMonthValue.innerHTML = "LKR 0";
+perAnnumValue.innerHTML = "LKR 0";
+
