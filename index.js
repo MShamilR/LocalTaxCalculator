73fa8fb472e6e69b54bb1ce0c91969808ba7c123
supremeincubator <mohammedshamil0300@gmail.com>
2022-10-24 19:23:25 +0530

    Added cleave.js and other gimmicks

--- index.js
+++ index.js
@@ -1,6 +1,43 @@
-const perMonthValue = document.querySelector(".monthValue");
-const perAnnumValue = document.querySelector(".annumValue");
+const perMonth = document.querySelector(".input-monthValue");
+const perAnnum = document.querySelector(".input-annumValue");
+/* const check = document.querySelector(".check");
+ */
+var cleave = new Cleave(".input-monthValue", {
+  numeral: true,
+  numeralThousandsGroupStyle: "lakh",
+  prefix: 'LKR '
+});
 
-perMonthValue.innerHTML = "LKR 0";
-perAnnumValue.innerHTML = "LKR 0";
+/* 
+ */
+var cleave = new Cleave(".input-annumValue", {
+  numeral: true,
+  numeralThousandsGroupStyle: "lakh",
+  prefix: 'LKR '
+});
+
+perMonth.onkeyup = function (e) {
+  if (perMonth.value === 'LKR ') {
+    perAnnum.value = 'LKR ';
+    return;
+  }
+ /*  const parsedValue = parseFloat(e.target.value.replace(/,/g, "")); */
+  const parsedValue = parseFloat(e.target.value.replace(/[^\d\.]*/g, ""));
+  const multipliedValue = parsedValue * 12;
+  perAnnum.value = `LKR `+`${multipliedValue.toLocaleString("en-IN")}`
+};
+
+perAnnum.onkeyup = function (e) {
+  if (perAnnum.value === 'LKR ') {
+    perMonth.value = 'LKR ';
+    return;
+  }
+  /* const parsedValue = parseFloat(e.target.value.replace(/,/g, "")); */
+  const parsedValue = parseFloat(e.target.value.replace(/[^\d\.]*/g, ""));
+  const dividedValue = parsedValue / 12;
+  perMonth.value = `LKR `+`${dividedValue.toLocaleString("en-IN")}`
+};
+
+
+
 
