e951c7a81abfaa61408c1397dd36e12a5c474687
supremeincubator <mohammedshamil0300@gmail.com>
2022-11-02 12:28:47 +0530

    CSS Skeleton done

--- index.js
+++ index.js
@@ -5,39 +5,82 @@ const perAnnum = document.querySelector(".input-annumValue");
 var cleave = new Cleave(".input-monthValue", {
   numeral: true,
   numeralThousandsGroupStyle: "lakh",
-  prefix: 'LKR '
+  prefix: "LKR ",
 });
 
-/* 
+/*
  */
 var cleave = new Cleave(".input-annumValue", {
   numeral: true,
   numeralThousandsGroupStyle: "lakh",
-  prefix: 'LKR '
+  prefix: "LKR ",
 });
 
 perMonth.onkeyup = function (e) {
-  if (perMonth.value === 'LKR ') {
-    perAnnum.value = 'LKR ';
+  if (perMonth.value === "LKR ") {
+    perAnnum.value = "LKR ";
     return;
   }
- /*  const parsedValue = parseFloat(e.target.value.replace(/,/g, "")); */
+  /*  const parsedValue = parseFloat(e.target.value.replace(/,/g, "")); */
   const parsedValue = parseFloat(e.target.value.replace(/[^\d\.]*/g, ""));
   const multipliedValue = parsedValue * 12;
-  perAnnum.value = `LKR `+`${multipliedValue.toLocaleString("en-IN")}`
+  /* window.annumNumberValue = multipliedValue; */
+  perAnnum.value = `LKR ` + `${multipliedValue.toLocaleString("en-IN")}`;
+  /* console.log(window.annumParsedValue) */
 };
 
 perAnnum.onkeyup = function (e) {
-  if (perAnnum.value === 'LKR ') {
-    perMonth.value = 'LKR ';
+  if (perAnnum.value === "LKR ") {
+    perMonth.value = "LKR ";
     return;
   }
   /* const parsedValue = parseFloat(e.target.value.replace(/,/g, "")); */
   const parsedValue = parseFloat(e.target.value.replace(/[^\d\.]*/g, ""));
   const dividedValue = parsedValue / 12;
-  perMonth.value = `LKR `+`${dividedValue.toLocaleString("en-IN")}`
+  /* window.annumParsedValue = parsedValue;
+  window.monthNumberValue = dividedValue; */
+  perMonth.value = `LKR ` + `${dividedValue.toLocaleString("en-IN")}`;
+  console.log(parsedValue);
 };
 
-
-
-
+const calculate = () => {
+  const annumValue = parseFloat(perAnnum.value.replace(/[^\d\.]*/g, ""));
+  console.log(annumValue)
+  const pAV = annumValue
+  let taxAmount = "";
+  if (pAV <= 1200000) {
+    console.log(
+      "You don't have to pay taxes as of now, Work hard until you have to pay taxes"
+    );
+  } else if (pAV <= 1700000) {
+    // First 0.5 mill
+    const taxable = pAV - 1200000;
+    taxAmount = taxable * 0.06;
+    console.log(taxAmount);
+  } else if (pAV <= 2200000) {
+    // Second 0.5 mill
+    const taxable = pAV - 1700000;
+    taxAmount = 30000 + taxable * 0.12;
+    console.log(taxAmount);
+  } else if (pAV <= 2700000) {
+    // Third 0.5 mill
+    const taxable = pAV - 2200000;
+    taxAmount = 30000 + 60000 + taxable * 0.18;
+    console.log(taxAmount);
+  } else if (pAV <= 3200000) {
+    // Fourth 0.5 mill
+    const taxable = pAV - 2700000;
+    taxAmount = 30000 + 60000 + 90000 + taxable * 0.24;
+    console.log(taxAmount);
+  } else if (pAV <= 3700000) {
+    // Fifth 0.5 mill
+    const taxable = pAV - 3200000;
+    taxAmount = 30000 + 60000 + 90000 + 120000 + taxable * 0.3;
+    console.log(taxAmount);
+  } else {
+    const taxable = pAV - 3700000;
+    taxAmount = 30000 + 60000 + 90000 + 120000 + 150000 + taxable * 0.36;
+    console.log(taxAmount);
+  }
+  pAV = "";
+};
